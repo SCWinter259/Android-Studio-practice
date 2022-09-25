ba5f471b168a5404c248e4f9f9b6b9b922d02d3f
SCWinter259 <catungthongkhi2002@gail.com>
2022-09-25 01:15:59 -0600

    done adding lab 3 cmput 301

---
+++ ListyCityLab3/CustomList.java
@@ -0,0 +1,49 @@
+package com.example.simpleparadox.listycity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class CustomList extends ArrayAdapter {
+    private ArrayList<City> cities;
+    private Context context;
+
+    public CustomList(Context context, ArrayList<City> cities) {
+        super(context, 0, cities);
+        this.cities = cities;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // The convertView object is a way to recycle old views inside the
+        // ListView ultimately increasing the performance of the ListView
+        // view here is used as a reference to the convertView object
+        View view = convertView;
+
+        // if convertView holds nothing, we inflate the content.xml
+        if(view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.content, parent, false);
+        }
+
+        // extract information and set information
+        City city = cities.get(position);
+
+        TextView cityName = view.findViewById(R.id.city_text);
+        TextView provinceName = view.findViewById(R.id.province_text);
+
+        cityName.setText(city.getCityName());
+        provinceName.setText(city.getProvinceName());
+
+        return view;
+    }
+}
---
+++ ListyCityLab3/Description.md
@@ -0,0 +1,9 @@
+# About
+
+Important files in lab 3, CMPUT 301, Fall 2022.
+
+# Specifications
+
+Add, and edit cities and corresponding provinces in a city-province list, using `FloatingActionButton` and `AlertDialog`
+- To add city, click floating action button at the right bottom of the screen. Alert dialog appears with 2 edit text fields, one for city and one for province. On the alert dialog are also a Cancel button and an OK button.
+- To edit city, click an item on the list. A similar alert dialog appears, along with the current city anf province name on the edit text fields. Edit those and click OK to edit city-province item.
